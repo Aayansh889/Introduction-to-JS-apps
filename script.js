f91db5e0b9dfeb2963eee722b116fab21146ae92
Aayansh889 <aayansh.saxena@gmail.com>
2025-07-22 16:28:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-var choice= prompt("Welcome to area calculator. /n Please enter your choice: /n1. Area of rectangle /n2.Area of triangle. /n3. Area of circle. /n4. Area of parellelogram")
+var choice= prompt("Welcome to perimeter calculator. /n Please enter your choice: /n1. Perimeter of rectangle /n2.Perimeter of triangle. /n3. Perimeter of circle. /n4. Perimeter of parellelogram")
 if (choice=='1') {
     var l= prompt("Enter the length")
     var b=prompt("Enter the breadth")
-    var result= Number(l) * Number(b)
+    var result= 2*[Number(l)+Number(b)]
     alert('The area is' + result)
 }
 if (choice=='2') {
     var h= prompt("Enter the height")
     var b=prompt("Enter the base")
-    var result= Number(h) * Number(b)/2
+    var result=Number(h) +Number(b) + Number(h)**2+Number(b)**2**0.5
     alert('The area is' + result)
 }
 if (choice=='3') {
     var r= prompt("Enter the radius")
-    var result= Number(r) * Number(r)*3.14
+    var result= 2*3.14*Number(r)
     alert('The area is' + result)
 }
 if (choice=='4') {
     var h= prompt("Enter the height")
     var cb=prompt("Enter the corresponding base")
-    var result= Number(h) * Number(cb)
+    var result= 2*[Number(h)+Number(cb)]
     alert('The area is' + result)
 }
